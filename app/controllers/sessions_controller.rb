431ccf3a5986ce970544fc5dba7477482573aefe
Jeremy Stoll <j8r8my@gmail.com>
2016-05-12 16:34:44 -0500

    modified sessions_controller, need destroy action for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     begin
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
-      flash[:success] = "Welcome, #{@user.name}!"
+      flash[:success] = "Welcome, #{@user.name}! Session Created."
     rescue
       flash[:warning] = "There was an error while trying to authenticate you..."
     end
